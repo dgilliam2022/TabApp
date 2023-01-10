89bbdc98f8e6bab12e418d12b0bc215df342cf9f
David <dave22011@yahoo.com>
2023-01-10 03:34:55 -0500

    Updates made after deploying the api to azure

--- AngularTabApp/src/app/Services/tabapp-logic.service.ts
+++ AngularTabApp/src/app/Services/tabapp-logic.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Cosminis } from '../Models/Entries';
+import { Entries } from '../Models/Entries'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TabappLogicService {
 
+  url : string = environment.api;
+
   constructor(private http: HttpClient) { }
+
+  getAllEntries() : Observable<Entries[]> {
+    return this.http.get(this.url + `viewEntries`) as Observable<Entries[]>;
+  }
 }
--- AngularTabApp/src/app/homepage/homepage.component.ts
+++ AngularTabApp/src/app/homepage/homepage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TabappLogicService } from '../Services/tabapp-logic.service';
+import { Entries } from '../Models/Entries';
 
 @Component({
   selector: 'app-homepage',
@@ -7,7 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tablogic: TabappLogicService, private router: Router) { }
+
+  //showEntries!:Promise<boolean>;
+  entries : Entries[] = [];
+
+  viewEntries() : void 
+  {
+      this.tablogic.getAllEntries().subscribe((res) => 
+      {
+        this.entries = res;
+        //this.showEntries=Promise.resolve(true);
+      })
+  }
 
   ngOnInit(): void {
   }
